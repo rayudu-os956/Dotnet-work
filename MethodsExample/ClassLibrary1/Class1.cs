5f00444c4664fa6c05b60671410d0c6e3578e587
glvsrayudu <67229268+rayudu-os956@users.noreply.github.com>
2025-10-21 09:36:48 +1100

    methods example

---
+++ MethodsExample/ClassLibrary1/Class1.cs
@@ -0,0 +1,109 @@
+﻿using System.ComponentModel.Design.Serialization;
+
+public class Product
+{
+    //fields
+
+    private int productID;
+    private string productName;
+    private double cost;
+
+    private double tax;
+    private int qunatityInStock;
+
+    public static int TotalNoProducts;
+
+    public const string CategoryName = "Electronics ";
+    private readonly string dateOfPurchase;
+
+    //constructor
+
+    public Product()
+    {
+        dateOfPurchase = System.DateTime.Now.ToShortDateString();
+    }
+    //set  
+    public void SetProductID(int value)
+    {
+        productID = value;
+
+    }
+    //get 
+    public int GetProductID()
+    {
+        return productID;
+    }
+        //set  
+    public void SetProductName(string value)
+    {
+        productName = value;
+
+    }
+    //get 
+    public string GetProducName()
+    {
+        return productName;
+    }
+            //set  
+    public void SetCost(double value)
+    {
+        cost = value;
+
+    }
+    //get 
+    public double GetCost()
+    {
+        return cost;
+    }
+
+                //set  
+    public void SetTax(double value)
+    {
+        tax = value;
+
+    }
+    //get 
+    public double GetTax()
+    {
+        return tax;
+    }
+
+     //set  
+    public void SetQuantityInStock(int value)
+    {
+        qunatityInStock = value;
+
+    }
+    //get 
+    public int GetQuantityInStock()
+    {
+        return qunatityInStock;
+    }
+
+    public string GetDateOfPurchase()
+    {
+        return dateOfPurchase;
+    }
+
+    //method 
+    //cost<=2000 then tax =10%
+    //cost >2000 then tax =12.5%
+
+    public void CaluclateTax()
+    {
+        double t;
+
+        //clauclate tax 
+        if (cost <= 20000)
+        {
+            t = cost * 10 / 100;
+        }
+        else
+        {
+            t = cost * 12.5 / 100;
+        }
+        tax = t;  
+    }
+
+}
+
---
+++ MethodsExample/MethodsExample/Program.cs
@@ -0,0 +1,85 @@
+﻿class Sample
+{
+    static void Main()
+    {
+        //create objects
+        Product product1, product2, product3;
+
+        //creating objects  
+        product1 = new Product();
+        Product.TotalNoProducts++;
+        product2 = new Product();
+        Product.TotalNoProducts++;
+        product3 = new Product();
+        Product.TotalNoProducts++;
+
+        //initializw fields
+
+        product1.SetProductID(1001);
+        product1.SetProductName("mobile");
+        product1.SetCost(20000);
+        product1.SetQuantityInStock(1200);
+        product2.SetProductID(1002);
+        product2.SetProductName("Laptop");
+        product2.SetCost(45000);
+        product2.SetQuantityInStock(3400);
+        product3.SetProductID(1003);
+        product3.SetProductName("Speakers");
+        product3.SetCost(36000);
+        product3.SetQuantityInStock(800);
+        //call methods
+
+        product1.CaluclateTax();
+        product2.CaluclateTax();
+        product3.CaluclateTax();
+
+        //get values from fields
+        System.Console.WriteLine("Product1:");
+        System.Console.WriteLine("ProductId:" + product1.GetProductID());
+        System.Console.WriteLine("Product Name:" + product1.GetProducName());
+        System.Console.WriteLine("Product Cost:" + product1.GetCost());
+        System.Console.WriteLine("Product Quantity:" + product1.GetQuantityInStock());
+        System.Console.WriteLine("Date of Purchase:" + product1.GetDateOfPurchase());
+        System.Console.WriteLine("Tax:" + product1.GetTax());
+
+        System.Console.WriteLine("\nProduct2:");
+        System.Console.WriteLine("ProductId:" + product2.GetProductID());
+        System.Console.WriteLine("Product Name:" + product2.GetProducName());
+        System.Console.WriteLine("Product Cost:" + product2.GetCost());
+        System.Console.WriteLine("Product Quantity:" + product2.GetQuantityInStock());
+        System.Console.WriteLine("Date of Purchase:" + product2.GetDateOfPurchase());
+        System.Console.WriteLine("Tax:" + product2.GetTax());
+
+        System.Console.WriteLine("\nProduct3:");
+        System.Console.WriteLine("ProductId:" + product3.GetProductID());
+        System.Console.WriteLine("Product Name:" + product3.GetProducName());
+        System.Console.WriteLine("Product Cost:" + product3.GetCost());
+        System.Console.WriteLine("Product Quantity:" + product3.GetQuantityInStock());
+        System.Console.WriteLine("Date of Purchase:" + product3.GetDateOfPurchase());
+        System.Console.WriteLine("Tax:" + product3.GetTax());
+
+        int totalQuantity = product1.GetQuantityInStock() + product2.GetQuantityInStock() + product3.GetQuantityInStock();
+        System.Console.WriteLine("Total Quantity:" + totalQuantity);
+         Product highestCostProduct = product1;
+        
+        if (product2.GetCost() > highestCostProduct.GetCost())
+        {
+            highestCostProduct = product2;
+        }
+
+        if (product3.GetCost() > highestCostProduct.GetCost())
+        {
+            highestCostProduct = product3;
+        }
+        Console.WriteLine("\nProduct with the highest cost:");
+        Console.WriteLine("Product ID: " + highestCostProduct.GetProductID());
+        Console.WriteLine("Product Name: " + highestCostProduct.GetProducName());
+        Console.WriteLine("Cost: " + highestCostProduct.GetCost());
+        Console.WriteLine("Quantity in Stock: " + highestCostProduct.GetQuantityInStock());
+
+        System.Console.WriteLine("Total no of Products:" + Product.TotalNoProducts);
+        System.Console.WriteLine("Category  of Products:" + Product.CategoryName);
+
+    }
+}
+
