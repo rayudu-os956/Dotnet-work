df0d19c9a7a0096ba3627d44a6d4c705fcb3bbf9
glvsrayudu <67229268+rayudu-os956@users.noreply.github.com>
2025-10-24 12:05:26 +1100

    ref returns

---
+++ RefReturnsExample/RefReturnsExample/Program.cs
@@ -0,0 +1,30 @@
+﻿class Student
+{
+    public int grade = 2;
+    public void PrintGrade()
+    {
+        System.Console.WriteLine("Grade:" + grade);
+    }
+    
+    public ref int DoWork()
+    {
+        return ref grade;
+    }
+
+}
+
+class Program
+{
+    static void Main()
+    {
+        Student s = new Student();
+        s.PrintGrade();
+        //call Do Wrork
+        ref int g = ref s.DoWork();
+        //update the value of 'ref return'
+        g = 5;
+        //call print after updating value of 'ref return'
+        s.PrintGrade();
+        
+    }
+}
