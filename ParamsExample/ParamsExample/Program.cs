3b4b48fa696187ce8337fc0d9ebdc751f5170d22
glvsrayudu <67229268+rayudu-os956@users.noreply.github.com>
2025-10-24 12:25:07 +1100

    params example

---
+++ ParamsExample/ParamsExample/Program.cs
@@ -0,0 +1,21 @@
+﻿class Student
+{
+    public void DisplaySubjects(params string[] subjcets)
+    {
+        for(int i=0;i<subjcets.Length;i++)
+        {
+            System.Console.WriteLine(subjcets[i]);
+        }
+
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Student s = new Student();
+        s.DisplaySubjects("Theory of Computation", "Computer Networks", "Discrete Mathematics", "Digital system Desgin","Basics of c");
+
+    }
+}
