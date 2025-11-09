2a61ed8e416c6697bea36408cfcfc12ddfb9cf32
glvsrayudu <67229268+rayudu-os956@users.noreply.github.com>
2025-11-10 10:24:20 +1100

    Type conversion

---
+++ TryParse/TryParse/Program.cs
@@ -0,0 +1,22 @@
+﻿class Program
+{
+    static void Main()
+    {
+        string s;
+        System.Console.WriteLine("Enter a number: ");
+        s = System.Console.ReadLine();
+
+        //tryparese
+        bool b = int.TryParse(s, out int n);
+
+        if (b == true)
+        {
+            System.Console.WriteLine("Conversion suceessful");
+            System.Console.WriteLine(n);
+        }
+        else
+        {
+            System.Console.WriteLine("Conversion failed");
+        }
+    }
+}
