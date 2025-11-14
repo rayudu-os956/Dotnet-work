5af3a7181c609cfab6cc4e798ba169dc4006da60
glvsrayudu <67229268+rayudu-os956@users.noreply.github.com>
2025-11-14 12:11:36 +1100

    polymorphism

--- inheritance/ClassLibrary1/obj/Debug/net9.0/ClassLibrary1.AssemblyInfo.cs
+++ inheritance/ClassLibrary1/obj/Debug/net9.0/ClassLibrary1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ClassLibrary1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+af2c25e12e1f5b3859975db0b004dbd323841d97")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+77aa3f12aff73a54e1b7854f52601cbfdb96d48c")]
 [assembly: System.Reflection.AssemblyProductAttribute("ClassLibrary1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ClassLibrary1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- inheritance/inheritance/Program.cs
+++ inheritance/inheritance/Program.cs
@@ -8,34 +8,42 @@
         // System.Console.WriteLine(emp1.EmpID);
         // System.Console.WriteLine(emp1.EmpName);
         // System.Console.WriteLine(emp1.Location);
+        //
+        //create reference variable pf  Employee class 
+
+        Employee emp;
+
 
         //create bject for manager
+        emp = new Manager(102,"Mook","Bangkok","Accounting");
+        System.Console.WriteLine(emp.GetHealthInsuranceAmount());
 
-        Manager mgr1 = new Manager(102,"Mook","Bangkok","Accounting");
+        // Manager mgr1 = new Manager(102,"Mook","Bangkok","Accounting");
  
-        System.Console.WriteLine("Object of child class(manager)");
-        System.Console.WriteLine(mgr1.EmpID);
-        System.Console.WriteLine(mgr1.EmpName);
-        System.Console.WriteLine(mgr1.Location);
-        System.Console.WriteLine(mgr1.DepartmentName);
-        System.Console.WriteLine(mgr1.GetTotalSalesOfTheYear());
-        System.Console.WriteLine(mgr1.GetFullDepartmentName());
-        System.Console.WriteLine(mgr1.GetHealthInsuranceAmount());
+        // System.Console.WriteLine("Object of child class(manager)");
+        // System.Console.WriteLine(mgr1.EmpID);
+        // System.Console.WriteLine(mgr1.EmpName);
+        // System.Console.WriteLine(mgr1.Location);
+        // System.Console.WriteLine(mgr1.DepartmentName);
+        // System.Console.WriteLine(mgr1.GetTotalSalesOfTheYear());
+        // System.Console.WriteLine(mgr1.GetFullDepartmentName());
+        // System.Console.WriteLine(mgr1.GetHealthInsuranceAmount());
 
         //Based type reference to same object 
        // Employee eRef = mgr1;
         //Console.WriteLine("Base ref to same maanger:" + eRef.GetHealthInsuranceAmount()); 
                //object for slaesman
 
-        Salesman salesman1 = new Salesman(103,"Dory","Bejing","East");
+        // Salesman salesman1 = new Salesman(103,"Dory","Bejing","East");
 
+        emp = new Salesman(103,"Dory","Bejing","East");
         System.Console.WriteLine("Object of child class(salesman)");
-        System.Console.WriteLine(salesman1.EmpID);
-        System.Console.WriteLine(salesman1.EmpName);
-        System.Console.WriteLine(salesman1.Location);
-        System.Console.WriteLine(salesman1.Region);
-        System.Console.WriteLine(salesman1.GetSalesOfTheCurrentMonth());
-        System.Console.WriteLine(salesman1.GetHealthInsuranceAmount());
+        // System.Console.WriteLine(salesman1.EmpID);
+        // System.Console.WriteLine(salesman1.EmpName);
+        // System.Console.WriteLine(salesman1.Location);
+        // System.Console.WriteLine(salesman1.Region);
+        // System.Console.WriteLine(salesman1.GetSalesOfTheCurrentMonth());
+        System.Console.WriteLine(emp.GetHealthInsuranceAmount());
 
 
 
