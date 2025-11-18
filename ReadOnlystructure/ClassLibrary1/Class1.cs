f2e94ede4a4670c6d0cfeb73bdaa881f2b5df4f5
glvsrayudu <67229268+rayudu-os956@users.noreply.github.com>
2025-11-18 16:13:16 +1100

    Readonlu structure

---
+++ ReadOnlystructure/ClassLibrary1/Class1.cs
@@ -0,0 +1,23 @@
+﻿public readonly struct Marvel
+{
+    private readonly string _characterName;
+
+    public string CharacterName
+    {
+        get
+        {
+            return _characterName;
+        }
+    }
+
+    public void PrintCharacterName()
+    {
+        System.Console.WriteLine(this.CharacterName);
+
+    }
+
+    public Marvel( string CharacterName)
+    {
+        this._characterName = CharacterName;
+    }
+}
---
+++ ReadOnlystructure/ReadOnlystructure/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        Marvel m = new Marvel("IRONMAN");
+        Console.WriteLine(m.CharacterName);
+        m.PrintCharacterName();
+    }
+}
