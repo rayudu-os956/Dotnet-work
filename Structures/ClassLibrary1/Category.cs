f55d53a63e3f277a18223b17179ce9f1b5779499
glvsrayudu <67229268+rayudu-os956@users.noreply.github.com>
2025-11-18 15:33:08 +1100

    structures

---
+++ Structures/ClassLibrary1/Category.cs
@@ -0,0 +1,37 @@
+﻿public struct Categroy
+{
+    private int _categoryID;
+    private string _categoryName;
+
+    public int CategoryID
+    {
+        set
+        {
+            if(value >=1 && value <= 100)
+            {
+                _categoryID  = value;
+            }
+        }
+        get
+        {
+            return _categoryID;
+        }
+    }
+    public string CategroyName
+    {
+        set
+        {
+            if (value.Length <= 40){
+             _categoryName = value;   
+            }
+        }
+        get
+        {
+            return _categoryName;
+        }
+    }
+    public int GetCategroyNameLength()
+    {
+        return this._categoryName.Length;
+    }
+}
---
+++ Structures/Structures/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        //create structure instance
+        Categroy categroy = new Categroy(); //its for intialised the struture not an object 
+        //inittialiswwe fields throough properties 
+        categroy.CategoryID =20;
+        categroy.CategroyName = "General";
+
+        //access methods 
+        Console.WriteLine(categroy.GetCategroyNameLength());
+        Console.WriteLine(categroy.CategoryID);
+        Console.WriteLine(categroy.CategroyName);
+
+    }
+}
