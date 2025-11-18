f0fd9364b41b8c9fd5a4dbd1893f70f00a56cf3f
glvsrayudu <67229268+rayudu-os956@users.noreply.github.com>
2025-11-18 11:31:33 +1100

    using static

--- Namespaces/ClassLibrary1/obj/Debug/net9.0/ClassLibrary1.AssemblyInfo.cs
+++ Namespaces/ClassLibrary1/obj/Debug/net9.0/ClassLibrary1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ClassLibrary1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+642ddd5545cd64c05e092e18c282c8c9675c7bd9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+736df951ba17bb765bef28b83d58adfeff46994c")]
 [assembly: System.Reflection.AssemblyProductAttribute("ClassLibrary1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ClassLibrary1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Namespaces/Namespaces/Program.cs
+++ Namespaces/Namespaces/Program.cs
@@ -1,18 +1,21 @@
 ﻿using HR;
 using m= HR.Mgr;
+using static System.Console;
 
 using FrontOffice;
 class Program
 {
     static void Main() 
     {
+        WriteLine("Magaer class");
         m.IMangager manager1;
          manager1 = new m.Manager();
         m.IMangager manager2;
          manager2 = new m.Manager();
         m.IMangager manager3;
          manager3 = new m.Manager();
-   
+        
+        WriteLine("Customer Enquiry  class");
          CustomerEnquiry customerEnquiry =   new CustomerEnquiry();
     }
 }
