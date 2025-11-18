6138225550d6b408ad05ca6d7af3795b343ee3e2
glvsrayudu <67229268+rayudu-os956@users.noreply.github.com>
2025-11-18 16:23:52 +1100

    Primitive types in struct

---
+++ PrimitiveTypesAsStructures/PrimitiveTypesAsStructures/Program.cs
@@ -0,0 +1,13 @@
+﻿class Program
+{
+    static void Main()
+    {
+        sbyte a =10;//structure instance
+
+        System.String b = "hello";//alias string type
+        //hello is object of string class 
+        //to maintain tyoe safety
+         
+
+    }
+}
