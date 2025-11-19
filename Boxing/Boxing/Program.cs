5d0852bde69b8f3d6b432e2764b298caa8ea2f2b
glvsrayudu <67229268+rayudu-os956@users.noreply.github.com>
2025-11-19 13:00:17 +1100

    Boxing

---
+++ Boxing/Boxing/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        int x =10;
+
+        //boxing (valur-type to refernce -type)
+        object obj =x;
+        Console.WriteLine(x);
+        Console.WriteLine(obj);
+
+
+    }
+}
