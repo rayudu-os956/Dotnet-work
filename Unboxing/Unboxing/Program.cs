aa354b4c784f85464e6234deba52f13251e29ef8
glvsrayudu <67229268+rayudu-os956@users.noreply.github.com>
2025-11-19 13:04:36 +1100

    unboxing

---
+++ Unboxing/Unboxing/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        //reference variable
+        object  obj =10;
+
+        //uboxing (refernce-type to value -type)
+        int x = (int)obj;
+        Console.WriteLine(x);
+        Console.WriteLine(obj);
+
+
+    }
+}
