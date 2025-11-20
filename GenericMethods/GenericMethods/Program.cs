1108a06fdcd1db66ac6a0c0a58722d06c2673650
glvsrayudu <67229268+rayudu-os956@users.noreply.github.com>
2025-11-20 14:50:12 +1100

    Generic methods

---
+++ GenericMethods/GenericMethods/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Diagnostics;
+
+class Program
+{
+    static void Main()
+    {
+        Sample sample = new Sample();
+        Employee emp = new Employee(){Salary =1000};
+        Student stu = new Student(){Marks = 80};
+
+        sample.PrintData<Employee>(emp);
+        sample.PrintData<Student>(stu);
+        
+    }
+}
