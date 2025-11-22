86afec33f512b2ad5f93fca543c54acca3e17f49
glvsrayudu <67229268+rayudu-os956@users.noreply.github.com>
2025-11-22 20:06:18 +1100

    null colaesing

--- NullableTypes/NullableTypes/Program.cs
+++ NullableTypes/NullableTypes/Program.cs
@@ -9,14 +9,11 @@
         static void Main()
         {
             Person p1 = new Person(){NoOfchildren =2};
-            Person p2 = new Person(){NoOfchildren = null};
+            Person p2 = new Person(){NoOfchildren = 3};
             Person p3 = null;
-
-            if (p2.NoOfchildren.HasValue)
-            {
-            Console.WriteLine(p1.NoOfchildren);
-            Console.WriteLine(p2.NoOfchildren);
-            }
+            //Console.WriteLine(p1.NoOfchildren);
+            Console.WriteLine(p2.NoOfchildren ??0);
+            
         }
     }
 }
--- NullableTypes/NullableTypes/obj/Debug/net9.0/NullableTypes.AssemblyInfo.cs
+++ NullableTypes/NullableTypes/obj/Debug/net9.0/NullableTypes.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NullableTypes")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0f968451218cb4a734c651b77bcb3790f50413af")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6b6ed73614c9944427f5aa226fad638722ec7036")]
 [assembly: System.Reflection.AssemblyProductAttribute("NullableTypes")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NullableTypes")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
