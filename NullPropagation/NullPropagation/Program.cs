d5c61de21de22bbdebcbd34c77bbfc1aae02e80e
glvsrayudu <67229268+rayudu-os956@users.noreply.github.com>
2025-11-22 20:19:55 +1100

    Null propagation

---
+++ NullPropagation/NullPropagation/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace NullPropagation
+{
+    class Person
+    {
+        public int Age;
+    }
+    class Program
+    {
+        static void Main()
+        {
+           // Person p1=new Person(){Age=20};
+           Person p1=null;
+            //
+            Console.WriteLine(p1?.Age);
+            
+        }
+    }
+    
+}
