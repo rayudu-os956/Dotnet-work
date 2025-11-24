152ed1e9e4934b948da6fe092564cda67ecba07a
glvsrayudu <67229268+rayudu-os956@users.noreply.github.com>
2025-11-24 21:59:20 +1100

    switchExpression

---
+++ SwitchExpresssion/SwitchExpresssion/Program.cs
@@ -0,0 +1,20 @@
+﻿class Program
+{
+    static void Main()
+    {
+        int operation =10;
+        string result;
+        //switch expression
+
+        result = operation switch
+        {
+            1 => "Customer",
+            2 => "student",
+            3 => "employee",
+            4 => "supplier",
+             _=> "No case avaialiable"
+
+        };
+        Console.WriteLine(result);
+    }
+}
