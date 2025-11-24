c66ae26e3a3bef202abc7fe001c67b4759e4a189
glvsrayudu <67229268+rayudu-os956@users.noreply.github.com>
2025-11-24 15:14:01 +1100

    InnerClass

---
+++ InnerClass/ClassLibrary1/Class1.cs
@@ -0,0 +1,25 @@
+﻿namespace ClassLibrary1;
+
+public class Student
+{
+    public double SecuredMarks;
+    public double MaxMarks;
+    public double Percentage;
+}
+//outer class
+public class MarksCalculation
+{
+    public void CalculatePercentage(Student s)
+    {
+        CalucaltionJelper ch = new CalucaltionJelper();
+        s.Percentage = ch.Multiply(s.SecuredMarks/s.MaxMarks,100);
+    }
+    //inner class 
+    public class CalucaltionJelper
+    {
+        public double Multiply(double n1,double n2)
+        {
+            return n1*n2;
+        }
+    }
+}
---
+++ InnerClass/InnerClass/Program.cs
@@ -0,0 +1,15 @@
+﻿using ClassLibrary1;
+
+class Program
+{
+    static void Main()
+    {
+        ClassLibrary1.MarksCalculation.CalucaltionJelper ch = new ClassLibrary1.MarksCalculation.CalucaltionJelper();
+        
+        Console.WriteLine(ch.Multiply(10,5));
+        ClassLibrary1.MarksCalculation mc = new ClassLibrary1.MarksCalculation();
+        Student s = new Student(){SecuredMarks =36, MaxMarks =50};
+        mc.CalculatePercentage(s);
+        Console.WriteLine(s.Percentage);
+    }
+}
