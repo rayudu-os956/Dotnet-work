695c230b64ee92776d184509faf6a5b2bcaf38dc
glvsrayudu <67229268+rayudu-os956@users.noreply.github.com>
2025-11-24 14:34:55 +1100

    ImplictVaraiables

---
+++ ImplicitlyTypedVariables/ClassLibrary1/Class1.cs
@@ -0,0 +1,15 @@
+﻿
+
+namespace namespace1
+{
+    namespace namespace2
+    {
+        namespace namespace3
+        {
+            public class Person
+            {
+                public string PersonName{get;set;}
+            }
+        }
+    }
+}
---
+++ ImplicitlyTypedVariables/ImplicitlyTypedVariables/Program.cs
@@ -0,0 +1,13 @@
+﻿
+class Progran
+{
+    static void Main()
+    {
+        
+       var p =  new namespace1.namespace2.namespace3.Person(){PersonName = "Rayudu"};
+       var p2 = "Rayudu".ToUpper();
+       Console.WriteLine(p.PersonName);
+       Console.WriteLine(p2);
+
+    }
+}
