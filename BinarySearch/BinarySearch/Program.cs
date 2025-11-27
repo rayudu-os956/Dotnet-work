d162ad6e596410c73952c4eb6de0ea40c3ad02f9
glvsrayudu <67229268+rayudu-os956@users.noreply.github.com>
2025-11-28 10:33:53 +1100

    binarysearch

---
+++ BinarySearch/BinarySearch/Program.cs
@@ -0,0 +1,16 @@
+﻿class Program
+{
+    static void Main()
+    {
+        double[] a = new double[]{10,20,30,40,50,60,70,80,90,100};
+
+        int n = Array.BinarySearch(a,30);
+        Console.WriteLine("30 is found at "+n);
+        //search for 30 in the array 
+
+        int n2 = Array.BinarySearch(a,80);
+          Console.WriteLine("100 is found at "+n2);
+
+
+    }
+}
