5008a5af79b0a958c6c10f9d5bffff89169db9a6
glvsrayudu <67229268+rayudu-os956@users.noreply.github.com>
2025-11-28 10:21:04 +1100

    recverse

---
+++ IndexOf/IndexOf/Program.cs
@@ -0,0 +1,10 @@
+﻿class Program
+{
+    static void Main()
+    {
+        double[] a = new double[5]{10,20,30,40,50};
+
+        int n = Array.IndexOf(a,30);
+        Console.WriteLine("30 is found at "+n);
+    }
+}
