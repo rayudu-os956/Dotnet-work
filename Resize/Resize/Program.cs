6e155f5708fbbf145081ef60568551df685a9b95
glvsrayudu <67229268+rayudu-os956@users.noreply.github.com>
2025-11-28 10:58:49 +1100

    resize

---
+++ Resize/Resize/Program.cs
@@ -0,0 +1,13 @@
+﻿class Program
+{
+    static void Main()
+    {
+        int[] a = new int[] {10,20,20};
+        Array.Resize(ref a,2);
+
+        foreach(var item in a)
+        {
+            Console.WriteLine(item);
+        }
+    }
+}
