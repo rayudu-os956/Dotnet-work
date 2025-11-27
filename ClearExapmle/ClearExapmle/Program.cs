d6dcf3abd9df55a818ec2e592ea7d94f4b54c575
glvsrayudu <67229268+rayudu-os956@users.noreply.github.com>
2025-11-28 10:39:57 +1100

    clear in arrays

---
+++ ClearExapmle/ClearExapmle/Program.cs
@@ -0,0 +1,12 @@
+﻿class Program
+{
+    static void Main()
+    {
+        int[] a = new int[] {10,20,20,40,50,60};
+        Array.Clear(a,2,3);
+        foreach (var item in a)
+        {
+            Console.WriteLine(item);
+        }
+    }
+}
