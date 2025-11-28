a6fe33b69c76a812e08c6aa1fb2880ebf965452e
glvsrayudu <67229268+rayudu-os956@users.noreply.github.com>
2025-11-28 11:18:37 +1100

    multi-dimmension-array

---
+++ MultiDimmensionalArrays/MultiDimmensionalArrays/Program.cs
@@ -0,0 +1,23 @@
+﻿class Program
+{
+    static void Main()
+    {
+        int[,] a = new int[4, 3]
+        {
+            {1,2,3},
+            {140,23,34},
+            {51,62,53},
+            {54,98,76}
+
+        };
+        for (int i = 0; i < 4; i++)
+        {
+            for(int j=0; j < 3; j++)
+            {
+                Console.Write(a[i,j]);
+                Console.Write(" ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
