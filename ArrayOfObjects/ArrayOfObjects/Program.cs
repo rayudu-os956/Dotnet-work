35cd69c8785692ff4fdc0ca8f2d5caab97165f91
glvsrayudu <67229268+rayudu-os956@users.noreply.github.com>
2025-11-28 11:57:44 +1100

    ArrayOfObjects

---
+++ ArrayOfObjects/ArrayOfObjects/Program.cs
@@ -0,0 +1,21 @@
+﻿using ClassLibrary1;
+
+class Program
+{
+    static void Main()
+    {
+
+
+        Employee[] employees = new Employee[]
+        {
+        new Employee(){empId = 101 , EmpName = "Rayudu"},
+        new Employee(){empId = 102 , EmpName = "Mook"},
+        new Employee(){empId = 103 , EmpName = "Dory"},
+        };
+        foreach(Employee item in employees)
+        {
+            Console.WriteLine(item.empId + " ," + item.EmpName);
+        }
+
+    }
+}
---
+++ ArrayOfObjects/ClassLibrary1/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace ClassLibrary1;
+
+public class Employee
+{
+    public int empId{set;get;}
+    public string EmpName{get;set;}
+}
