fd8c04dd7aee9a13219ae11de9faec126f512905
glvsrayudu <67229268+rayudu-os956@users.noreply.github.com>
2025-11-28 11:03:10 +1100

    array sorting

---
+++ Sort/Sort/Program.cs
@@ -0,0 +1,12 @@
+﻿class Program
+{
+    static void Main()
+    {
+        int[] a = new int[]{56,98,65,5,4,100,900,124};
+        Array.Sort(a);
+        foreach (var item in a)
+        {
+            Console.WriteLine(item);
+        }
+    }
+}
