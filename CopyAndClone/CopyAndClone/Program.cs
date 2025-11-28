ea18d633ba150ab6cb8e4edc8573afbbc456a1a4
glvsrayudu <67229268+rayudu-os956@users.noreply.github.com>
2025-11-28 12:28:06 +1100

    copyandclone

---
+++ CopyAndClone/CopyAndClone/Program.cs
@@ -0,0 +1,49 @@
+﻿class Program
+{
+    static void Main()
+    {
+        Employee[] employees = new Employee[]
+        {
+            new Employee(){EmployeeName="Rayudu",Role = "Developer"},
+            new Employee(){EmployeeName="Mook",Role = "Designer"},
+            new Employee(){EmployeeName = "Dory",Role = "Analyst"}
+        };
+        //new array 
+        Employee[]highlyPaidEmployees=new Employee[5];
+        employees.CopyTo(highlyPaidEmployees,2);
+          Console.WriteLine("Cpy:");
+        foreach (Employee emp in highlyPaidEmployees)
+        {
+            if(!(emp is null))
+            {
+                Console.WriteLine(emp.EmployeeName+"," +emp.Role); 
+            }
+            else
+            {
+                 Console.WriteLine("Null exception");
+            }
+           
+        }
+
+        //clone
+       Employee[] highlyPaidEmployees2 =  (Employee[])employees.Clone();
+       Console.WriteLine("\nClone:");
+       foreach(Employee emp in highlyPaidEmployees2)
+        {
+
+            if(!(emp is null))
+            {
+                Console.WriteLine(emp.EmployeeName+"," +emp.Role); 
+            }
+            else
+            {
+                 Console.WriteLine("Null exception");
+            }
+        }
+    }
+}
+class Employee
+{
+    public string EmployeeName{get;set;}
+    public string Role {get;set;}
+}
